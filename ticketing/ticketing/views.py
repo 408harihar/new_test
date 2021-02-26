b6660761ac40a9af25a4567f1cb69b071d040a3c
408harihar <408harihar@gmail.com>
2021-02-26 21:03:00 +0545

    first commit

---
+++ ticketing/ticketing/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Ticket
+from django.contrib import messages
+
+
+def home(request):
+    return render(request, 'tickets/home.html')
+
+
+def submit(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        body = request.POST.get('body')
+        if username is not '' and body is not '':
+            new_ticket = Ticket(submitter=username, body = body)
+            new_ticket.save()
+            messages.success(request, "your message is submitted")
+        else:
+            messages.error(request,"please imput both fields")
+
+    return render(request, 'tickets/submit.html')
+
+
+def tickets(request):
+    allTickets = Ticket.objects.all()
+    return render(request, 'tickets/tickets.html', {'tickets': allTickets})
