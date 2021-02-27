312e7bcd5d660074a735194be2017cf282d77fad
408harihar <408harihar@gmail.com>
2021-02-27 18:40:14 +0545

    hacker

--- ticketing/ticketing/models.py
+++ ticketing/ticketing/models.py
@@ -5,3 +5,4 @@ class Ticket(models.Model):
     submitter = models.CharField(max_length=100)
     body = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    hacker = models.CharField(max_length=100, blank=True)
--- ticketing/ticketing/views.py
+++ ticketing/ticketing/views.py
@@ -12,12 +12,13 @@ def submit(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         body = request.POST.get('body')
+        hacker = request.POST.get('hacker')
         if username is not '' and body is not '':
-            new_ticket = Ticket(submitter=username, body = body)
+            new_ticket = Ticket(submitter=username, body=body, hacker=hacker)
             new_ticket.save()
             messages.success(request, "your message is submitted")
         else:
-            messages.error(request,"please imput both fields")
+            messages.error(request, "please input both fields")
 
     return render(request, 'tickets/submit.html')
 
